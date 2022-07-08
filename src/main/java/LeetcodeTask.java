7ed41c4d7d6bd750d3b7817e4a513c4849a9deb0
MRiabov <maksymriabov2004@gmail.com>
2022-07-08 13:07:09 +0100

    how the bloody hell did people solve this 772 times????

--- src/main/java/LeetcodeTask.java
+++ src/main/java/LeetcodeTask.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class LeetcodeTask {
+public class LeetcodeTask {
+
+
+    public static void main(String[] args) {
+        int[] i=getSumAbsoluteDifferences(new int[]{2, 3, 5});
+    }
+
+    public static int[] getSumAbsoluteDifferences(int[] nums) {
+        int[] result = new int[nums.length];
+        int length=nums.length;
+
+
+        for (int i = 0; i < nums.length; i++) {
+
+        }
+
+        for (int j = 0; j < i; i++){
+            result[i]=nums[i]*i-nums[j]
+        }
+        return result;
+    }
+
 }
+/*
+f
+          
+ */
