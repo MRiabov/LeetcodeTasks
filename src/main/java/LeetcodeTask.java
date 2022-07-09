7175d12ed0f86799a05d4e47b5a706b782f5f376
MRiabov <maksymriabov2004@gmail.com>
2022-07-09 20:01:25 +0100

    OK, Leetcode 75 lessgooo

--- src/main/java/LeetcodeTask.java
+++ src/main/java/LeetcodeTask.java
@@ -2,27 +2,15 @@ public class LeetcodeTask {
 
 
     public static void main(String[] args) {
-        int[] i = getSumAbsoluteDifferences(new int[]{2, 3, 5});
+    }
+    public int[] runningSum(int[] nums) {
+        int[] sum = new int[nums.length];
+        sum[1]=nums[1];
+        for (int i = 1; i < nums.length; i++) {
+            sum[i]+=sum[i-1]+nums[i];
+        }
+        return sum;
     }
 
-    public static int[] getSumAbsoluteDifferences(int[] nums) {
-        int[] result = new int[nums.length];
-        int length = nums.length;
-        int[] prefixSum = new int[length];
-        int[] postfixSum = new int[length];
-
-        for (int i = 1; i < length - 1; i++) {
-            prefixSum[i]=prefixSum[i-1]+nums[i-1];
-        }
-
-        for (int i = length-2; i >= 0 ; i--) {
-            postfixSum[i]=postfixSum[i+1]+nums[i+1];
-        }
-
-        for (int i = 0; i < length; i++) {
-            result[i] = (nums[i] * i) - prefixSum[i]+ postfixSum[i]+(length-1-i)*nums[i];
-        }
-        return result;
-    }
 }
 
