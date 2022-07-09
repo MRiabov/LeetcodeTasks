b146af703b584e647866caa1be91d25282110666
MRiabov <maksymriabov2004@gmail.com>
2022-07-09 21:00:08 +0100

    Leetcode level 1 day1. Should this be that difficult or am I just stupid?

--- src/main/java/LeetcodeTask.java
+++ src/main/java/LeetcodeTask.java
@@ -2,14 +2,26 @@ public class LeetcodeTask {
 
 
     public static void main(String[] args) {
+        pivotIndex(new int[]{-1,-1,0,1,1,0});
     }
-    public int[] runningSum(int[] nums) {
-        int[] sum = new int[nums.length];
-        sum[1]=nums[1];
-        for (int i = 1; i < nums.length; i++) {
-            sum[i]+=sum[i-1]+nums[i];
+    public static int pivotIndex(int[] nums) {
+        int length=nums.length;
+        int[] sum = new int[length];
+        int[] reverseSum = new int[length];
+
+        for (int i = 1; i < length; i++) {
+            sum[i]=sum[i-1]+nums[i-1];
         }
-        return sum;
+        for (int i = length-2; i >= 0; i--) {
+            reverseSum[i]= reverseSum[i+1]+nums[i+1];
+        }
+        for (int i = 0; i < length; i++) {
+            if (sum[i]==reverseSum[i]) {
+                System.out.println(i);
+                return i;
+            }
+        }
+        return -1;
     }
 
 }
