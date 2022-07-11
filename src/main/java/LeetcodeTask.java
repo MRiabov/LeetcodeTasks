7bcff10514f29e74f0e127fbb1df5586e07956b8
MRiabov <maksymriabov2004@gmail.com>
2022-07-11 13:55:14 +0100

    this should work, but it just doesn't, idk v2

--- src/main/java/LeetcodeTask.java
+++ src/main/java/LeetcodeTask.java
@@ -16,23 +16,25 @@ public class LeetcodeTask {
     public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         ListNode currentNode=new ListNode();
         ListNode output = currentNode;
-        ListNode futureNode=new ListNode();
 
-        while (list1!=null&&list2!=null){
-
-           if (list1.val<=list2.val) {
-               currentNode.val=list1.val;
-               currentNode.next=new ListNode(list2.val,new ListNode());
-           } else {
-               currentNode.val=list2.val;
-               currentNode.next=new ListNode(list1.val,new ListNode());
-           }
-            currentNode=currentNode.next.next;
-           list1=list1.next;
-           list2=list2.next;
-       }
-       return output;
-    }
+        while (list1!=null||list2!=null){
+            if (list1==null) {
+                currentNode.next = list2;
+                list2=list2.next;
+            } else if (list2==null){
+                currentNode.next=list1;
+                list1=list1.next;
+            } else if (list1.val>= list2.val){
+                currentNode.next=list2;
+                list1=list1.next;
+            } else {
+                currentNode.next=list1;
+                list2=list2.next;
+            }
+            currentNode=currentNode.next;
+        }
+        return output.next;
+}
 }
 
 class ListNode {
