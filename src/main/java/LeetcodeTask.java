502d7077480b3d764ff2a7208ea2861c6ba96b8a
MRiabov <maksymriabov2004@gmail.com>
2022-07-13 09:49:52 +0100

    Forgot to commit earlier, so here ya go.

--- src/main/java/LeetcodeTask.java
+++ src/main/java/LeetcodeTask.java
@@ -1,31 +1,15 @@
 import java.util.HashMap;
 
 public class LeetcodeTask {
-
-
     public static void main(String[] args) {
-        middleNode(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))));
     }
 
-    public ListNode detectCycle(ListNode head) {
-        if(head == null || head.next == null) {
-            return null;
+    public int longestPalindrome(String s) {
+        HashMap <
+        for (int i = 0; i < s.length(); i++) {
+
         }
-        if(head.next.val == Integer.MAX_VALUE) {
-            return head.next;
-        }
-        int temp = head.val;
-        head.val = Integer.MAX_VALUE;
-        var result = detectCycle(head.next);
-        head.val = temp;
-        return result;
     }
 }
 
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }}
 
--- src/test/java/LeetcodeTaskTest.java
+++ src/test/java/LeetcodeTaskTest.java
@@ -1,5 +1,9 @@
+import com.sun.tools.javac.Main;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LeetcodeTaskTest {
+    @Test
 
 }
