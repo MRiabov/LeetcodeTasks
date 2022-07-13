dd136ee738742f0cf6de943aba83bc8f8c21d484
MRiabov <maksymriabov2004@gmail.com>
2022-07-13 18:28:19 +0100

    why are streams that unoptimized??? Like i literally solved the task with streams in over 40ms. people have done it in 2ms. ???

--- src/main/java/LeetcodeTask.java
+++ src/main/java/LeetcodeTask.java
@@ -1,15 +1,23 @@
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.stream.Collectors;
 
 public class LeetcodeTask {
     public static void main(String[] args) {
     }
 
     public int longestPalindrome(String s) {
-        HashMap <
-        for (int i = 0; i < s.length(); i++) {
-
-        }
+        HashMap <Character,Integer> hashMap = new HashMap<>();
+        boolean uneven;
+        s.chars()
+                .collect(Collectors.groupingBy(e -> e, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .map(e->e.getValue())
+                .
     }
+
+
 }
 
 
