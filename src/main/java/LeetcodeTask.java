ce713ca790fe31c0b92354d70d17ec747631d653
MRiabov <maksymriabov2004@gmail.com>
2022-07-15 10:19:32 +0100

    I'm getting sick of it, i can't solve a single task. perhaps it's the way of self-taught beginners...

--- src/main/java/LeetcodeTask.java
+++ src/main/java/LeetcodeTask.java
@@ -6,47 +6,18 @@ public class LeetcodeTask {
     public static void main(String[] args) {
     }
 
-
-
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> res = new ArrayList<>();
-        if(root == null) return res;
-        int level = 0;
-        Queue<Pair<TreeNode,Integer>> q = new LinkedList<>();
-        q.offer(new Pair(root,level));
-        List<Integer> temp = new ArrayList<>();
-        while(!q.isEmpty()){
-            while(q.peek() != null && q.peek().getValue() == level){
-                TreeNode t = q.poll().getKey();
-                temp.add(t.val);
-                if(t.left != null) q.offer(new Pair(t.left, level+1));
-                if(t.right != null) q.offer(new Pair(t.right, level+1));
+    public int search(int[] nums, int target) {
+        int lookup = nums.length / 2;
+        while (nums[lookup] != target)
+            if (nums[lookup] == target) return lookup;
+            else if (nums[lookup] > target) {
+                lookup /= 2;
+                search(nums, target);
+            } else {
+                lookup/=2;
+                search(nums, target);
             }
-            res.add(temp);
-            temp = new ArrayList<>();
-            level++;
-        }
-        return res;
-    }
     }
 }
 
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
