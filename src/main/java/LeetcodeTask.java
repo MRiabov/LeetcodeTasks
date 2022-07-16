be93b9e138999d5d7f99b31b6c2a1874fd6c2e55
MRiabov <maksymriabov2004@gmail.com>
2022-07-16 13:08:26 +0100

    Cool task. I even did this with 0 extra variables. Cool!

--- src/main/java/LeetcodeTask.java
+++ src/main/java/LeetcodeTask.java
@@ -1,29 +1,33 @@
-import java.util.*;
-import java.util.Random;
 public class LeetcodeTask {
 
     public static void main(String[] args) {
 
     }
-    public boolean isValidBST(TreeNode root) {
-        return validBST(root,Integer.MIN_VALUE,Integer.MAX_VALUE);
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        //repeat until they come at a node where one will be bigger and one will be smaller.
+        while (root!=null) {
+            if (q.val<root.val && p.val<root.val) root=root.left;
+            else if (q.val>root.val && p.val>root.val) root=root.right;
+            else return root;
+        }
+        return null;
     }
 
-    private boolean validBST(TreeNode root,int min,int max){
-        if (root == null) return true;
-        if (root.val<=min||root.val>=max) return false;
-        return validBST(root,min,root.val)&&validBST(root,root.val,max);
-    }
-    
-    
 }
 
 class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
     TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
