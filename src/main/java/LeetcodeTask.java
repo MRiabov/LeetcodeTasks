09e718f06f81d6baecf9489eaf1a3bf253afb23c
MRiabov <maksymriabov2004@gmail.com>
2022-07-17 12:09:26 +0100

    Weird errors. '1' wasn't equal to '1'. Well, I solved it myself, and I'm happy.

--- src/main/java/LeetcodeTask.java
+++ src/main/java/LeetcodeTask.java
@@ -1,28 +1,36 @@
 public class LeetcodeTask {
 
     public static void main(String[] args) {
-        floodFill(new int[][]{{1,1,1},{1,1,0},{1,0,1}},1,1,2);
+
     }
 
-    public static int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        LeetcodeTask.image = image;
-        recursionFill(sr, sc, color, image[sr][sc]);
-        return LeetcodeTask.image;
+
+    public int numIslands(char[][] grid) {
+        int result = 0;
+        foundGrid = new boolean[grid.length][grid[0].length];
+        for (int i = 0; i < foundGrid.length; i++) {
+            for (int j = 0; j < foundGrid[0].length; j++) {
+                if (grid[i][j]=='\u0001' && !foundGrid[i][j]) {
+                    result++;
+                    lookup(grid, i, j);
+                }
+            }
+        }
+        return result;
     }
 
-    static int[][] image;
+    boolean[][] foundGrid;
 
-    private static void recursionFill(int posX, int posY, int color, int startColor) {
-        if (image[posX][posY]!=color&&image[posX][posY] == startColor) {
-            image[posX][posY] = color;
-            if (posX+1!=image.length) recursionFill(posX+1, posY, color, startColor);
-            if (posX-1!=-1) recursionFill(posX-1, posY, color, startColor);
-            if (posY+1!=image[0].length) recursionFill(posX, posY+1, color, startColor);
-            if (posY-1!=-1) recursionFill(posX, posY-1, color, startColor);
+    private void lookup(char[][] grid, int posX, int posY) {
+        if (grid[posX][posY] == '1' && !foundGrid[posX][posY]) {
+            foundGrid[posX][posY] = true;
+            if (posX + 1 != grid.length) lookup(grid, posX + 1, posY);
+            if (posX != 0) lookup(grid, posX - 1, posY);
+            if (posY + 1 != grid[0].length) lookup(grid, posX, posY + 1);
+            if (posY != 0) lookup(grid, posX, posY - 1);
         }
     }
 }
-
 class TreeNode {
     int val;
     TreeNode left;
