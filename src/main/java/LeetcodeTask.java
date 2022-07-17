f8b33fee7ee872962e117326f2d9e29d43cc35a2
MRiabov <maksymriabov2004@gmail.com>
2022-07-17 09:27:32 +0100

    I thought I'd solve this quickly.. sure.

--- src/main/java/LeetcodeTask.java
+++ src/main/java/LeetcodeTask.java
@@ -1,19 +1,29 @@
+import java.util.Arrays;
+
 public class LeetcodeTask {
 
     public static void main(String[] args) {
-
+        floodFill(new int[][]{{1,1,1},{1,1,0},{1,0,1}},1,1,2);
     }
 
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        //repeat until they come at a node where one will be bigger and one will be smaller.
-        while (root!=null) {
-            if (q.val<root.val && p.val<root.val) root=root.left;
-            else if (q.val>root.val && p.val>root.val) root=root.right;
-            else return root;
-        }
-        return null;
+    public static int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        LeetcodeTask.image = image;
+        recursionFill(sr, sc, color, image[sr][sc]);
+        System.out.println(Arrays.deepToString(image));
+        return LeetcodeTask.image;
     }
 
+    static int[][] image;
+
+    private static boolean recursionFill(int posX, int posY, int color, int startColor) {
+        if (posX != 3 || posX != -1 || posY != 3 || posY != -1 || image[posX][posY] == startColor) {
+            image[posX][posY] = color;
+            return recursionFill(posX+1, posY, color, startColor)&&
+                    recursionFill(posX-1, posY, color, startColor)&&
+                    recursionFill(posX, posY+1, color, startColor)&&
+                    recursionFill(posX, posY-1, color, startColor);
+        } else return true;
+    }
 }
 
 class TreeNode {
