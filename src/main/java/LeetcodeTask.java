6d2317ef9ca3e3842c287dda431215c21edf3c02
MRiabov <maksymriabov2004@gmail.com>
2022-07-18 09:08:58 +0100

    Oh well, that was easy.

--- src/main/java/LeetcodeTask.java
+++ src/main/java/LeetcodeTask.java
@@ -5,30 +5,16 @@ public class LeetcodeTask {
     }
 
 
-    public int numIslands(char[][] grid) {
-        int result = 0;
-        foundGrid = new boolean[grid.length][grid[0].length];
-        for (int i = 0; i < foundGrid.length; i++) {
-            for (int j = 0; j < foundGrid[0].length; j++) {
-                if (grid[i][j]=='\u0001' && !foundGrid[i][j]) {
-                    result++;
-                    lookup(grid, i, j);
-                }
-            }
+    public int fib(int n) {
+        int num1=0;
+        int num2=1;
+        int fibonacci=1;
+        for (int i=1;i<n;i++){
+            fibonacci=num1+num2;
+            num1=num2;
+            num2=fibonacci;
         }
-        return result;
-    }
-
-    boolean[][] foundGrid;
-
-    private void lookup(char[][] grid, int posX, int posY) {
-        if (grid[posX][posY] == '1' && !foundGrid[posX][posY]) {
-            foundGrid[posX][posY] = true;
-            if (posX + 1 != grid.length) lookup(grid, posX + 1, posY);
-            if (posX != 0) lookup(grid, posX - 1, posY);
-            if (posY + 1 != grid[0].length) lookup(grid, posX, posY + 1);
-            if (posY != 0) lookup(grid, posX, posY - 1);
-        }
+        return fibonacci;
     }
 }
 class TreeNode {
