b8448a49820efe577226f9d304821403e7c512dd
MRiabov <maksymriabov2004@gmail.com>
2022-07-19 13:55:09 +0100

    oh yeah, i forgot to commit the last task Now I solve a really hard one.

--- src/main/java/LeetcodeTask.java
+++ src/main/java/LeetcodeTask.java
@@ -3,21 +3,26 @@ public class LeetcodeTask {
     public static void main(String[] args) {
 
     }
-    public int minCostClimbingStairs(int[] cost) {
-        return minCost(cost);
-    }
 
-    public int minCost(int[] cost){
-        int n = cost.length;
-        int one = cost[n-1];
-        int two = cost[n-2];
-
-        for(int i = n-3; i>=0 ; i-- ){
-            int temp = two;
-            two = cost[i] + Math.min(one,two);
-            one = temp;
+    public int calculateMinimumHP(int[][] dungeon) {
+        //[0][j]&[i][0] are a sum of [0][j-1] and [i-1][0] respectively
+        //we go from the end
+        int[][] dp = new int[dungeon[0].length][dungeon.length];
+        dp[dungeon[0].length][dungeon.length] = dungeon[dungeon[0].length][dungeon.length];
+        for (int i = dungeon.length-1; i > 0; i--) {
+            dp[i][0] = dp[i - 1][0] + dungeon[i][0];
         }
-
-        return Math.min(one,two);
+        for (int i = dungeon[0].length; i > 0; i--) {
+            dp[dungeon[0].length][i] = dp[dungeon[0].length][i + 1] + dungeon[dungeon[0].length][i];
+        }
+        //health may not go higher than 0, because the knight doesn't care about the bonus when he dies.
+        //then this room's most optimal is the min of bottom and right room.
+        for (int i = dungeon.length-1; i > 0; i--) {
+            for (int j = dungeon[0].length-1; j > 0; j--) {
+                dp[i][j] = Math.max(0, (Math.max(dp[i - 1][j], dp[i][j - 1])+dungeon[i][j]));
+            }
+        }
+        return -dp[dungeon.length - 1][dungeon[0].length - 1]+1;
     }
 }
+
