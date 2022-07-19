5a6fa76fb2d35f6a62684f8a162aba55bb4c7426
MRiabov <maksymriabov2004@gmail.com>
2022-07-19 11:07:53 +0100

    as any task on LeetCode, I'm pretty sure there is a one-line solution in Python for this.

--- src/main/java/LeetcodeTask.java
+++ src/main/java/LeetcodeTask.java
@@ -3,37 +3,21 @@ public class LeetcodeTask {
     public static void main(String[] args) {
 
     }
+    public int minCostClimbingStairs(int[] cost) {
+        return minCost(cost);
+    }
 
+    public int minCost(int[] cost){
+        int n = cost.length;
+        int one = cost[n-1];
+        int two = cost[n-2];
 
-    public int climbStairs(int n) {
-        int num1=1;
-        int num2=1;
-        int result=2;
-        if (n==1) return 1;
-        for (int i = 1; i < n; i++) {
-            result=num1+num2;
-            num1=num2;
-            num2=result;
+        for(int i = n-3; i>=0 ; i-- ){
+            int temp = two;
+            two = cost[i] + Math.min(one,two);
+            one = temp;
         }
-        return result;
+
+        return Math.min(one,two);
     }
 }
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-
-    TreeNode() {
-    }
-
-    TreeNode(int val) {
-        this.val = val;
-    }
-
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
-
