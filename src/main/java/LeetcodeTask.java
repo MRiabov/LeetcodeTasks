193e808db31489985b2bd819e6930d056d068203
MRiabov <maksymriabov2004@gmail.com>
2022-07-20 11:45:55 +0100

    And rewrite it once again!

--- src/main/java/LeetcodeTask.java
+++ src/main/java/LeetcodeTask.java
@@ -1,47 +1,63 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class LeetcodeTask {
 
     public static void main(String[] args) {
 
     }
 
-    public int calculateMinimumHP(int[][] dungeon) {
-        int[][] dp = new int[dungeon.length][dungeon[0].length];
-        dp[dungeon.length - 1][dungeon[0].length - 1] = dungeon[dungeon.length - 1][dungeon[0].length - 1];
-        for (int i = dungeon.length - 1; i > 0; i--) {
-            dp[i][dungeon[0].length - 1] = (dungeon[i][dungeon[0].length - 1] + dp[i - 1][dungeon[0].length - 1]);
-        }
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> result = new ArrayList<>();
+        int matchingLetterCount = 0;
+        char[] pChar = p.toCharArray();
 
-        for (int i = dungeon[0].length - 1; i > 0; i--) {
-            dp[dungeon.length - 1][i] = dungeon[dungeon.length - 1][i] + dp[dungeon.length - 1][i - 1];
-        }
-
-        for (int i = dungeon.length-2; i >= 0; i--) {
-            for (int j = dungeon[0].length-2; j >= 0; j--) {
-                dp[i][j] = dungeon[i][j]+Math.max(0,Math.max(dp[i+1][j],dp[i][j+1]));
+        for (int i = 0; i < s.length() - p.length(); i++) {
+            for (int k = 0; k < p.length(); k++) {
+                boolean letterMatches = false;
+                for (int l = 0; l < p.length(); l++) {
+                    if (s.charAt(i + k) == pChar[l]) {
+                        pChar[l] = '\u0000';
+                        matchingLetterCount++;
+                        letterMatches = true;
+                        break;
+                    }
+                }
+                if (!letterMatches) break;
             }
+            if (matchingLetterCount == p.length()) result.add(i);
+            matchingLetterCount = 0;
+            pChar = p.toCharArray();
         }
-        return 1-dp[0][0];
-    }
 
-//    public int calculateMinimumHP(int[][] dungeon) {
-//        //[0][j]&[i][0] are a sum of [0][j-1] and [i-1][0] respectively
-//        //we go from the end
-//        int[][] dp = new int[dungeon.length][dungeon[0].length];
-//        dp[dungeon[0].length][dungeon.length] = dungeon[dungeon[0].length][dungeon.length];
-//        for (int i = dungeon.length-1; i > 0; i--) {
-//            dp[i][0] = dp[i - 1][0] + dungeon[i][0];
-//        }
-//        for (int i = dungeon[0].length; i > 0; i--) {
-//            dp[dungeon[0].length][i] = dp[dungeon[0].length][i + 1] + dungeon[dungeon[0].length][i];
-//        }
-//        //health may not go higher than 0, because the knight doesn't care about the bonus when he dies.
-//        //then this room's most optimal is the min of bottom and right room.
-//        for (int i = dungeon.length-1; i > 0; i--) {
-//            for (int j = dungeon[0].length-1; j > 0; j--) {
-//                dp[i][j] = Math.max(0, (Math.max(dp[i - 1][j], dp[i][j - 1])+dungeon[i][j]));
-//            }
-//        }
-//        return -dp[dungeon.length - 1][dungeon[0].length - 1]+1;
-//    }
+        return result;
+    }
 }
+
+//        if (letter==s.charAt(i)){
+//                    p
+//                    s.getChars(i+1,i+pChar.length-1,sChar,0);
+//                    //if set of letters equals...
+
+//for (int i = 0; i < s.length(); i++) {
+//        for (int j = 0; j < p.length(); j++) {
+//            if (s.charAt(i)==pChar[j]){
+//                if (matchingLetterCount==0) currentAnagram=i;
+//                matchingLetterCount++;
+//                pChar[j]='\u0000';
+//                break;
+//            }
+//
+//        }
+//        if (matchingLetterCount==p.length()){
+//            result.add(currentAnagram);
+//            matchingLetterCount=0;
+//            pChar=p.toCharArray();
+//        } else if (i==currentAnagram+p.length()) {
+//            pChar=p.toCharArray();
+//            matchingLetterCount=0;
+//        }
+//    }
+//        return result;
+//}
 
--- src/test/java/LeetcodeTaskTest.java
+++ src/test/java/LeetcodeTaskTest.java
@@ -1,15 +1,20 @@
-import com.sun.tools.javac.Main;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LeetcodeTaskTest {
     LeetcodeTask leetcodeTask = new LeetcodeTask();
 
 
 
-    @Test
-    void fib() {
 
+    @Test
+    void findAnagrams() {
+        ArrayList<Integer> result = new ArrayList<>();
+        result.add(0);
+        result.add(6);
+        assertEquals(result, leetcodeTask.findAnagrams("cbaebabacd","abc"));
     }
 }
