72e95abbb202d188cb8818b6c1f5b4ceadfbd8a7
MRiabov <maksymriabov2004@gmail.com>
2022-07-24 16:37:02 +0100

    Bloody hell, its 16:30 and I still have one task to go. Impossible!

--- src/main/java/LeetcodeTask.java
+++ src/main/java/LeetcodeTask.java
@@ -1,38 +1,10 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Stack;
+import java.util.*;
 
 public class LeetcodeTask {
 
-    public static void main(String[] args) {
 
-    }
-//        char[] secretChar = secret.toCharArray();
-//        char[] guessChar = guess.toCharArray();
+    public int[] findBall(int[][] grid) {
 
-    public String decodeString(String s) {
-        StringBuilder stringBuilder = new StringBuilder()//needs to be used in midResult
-        String midResult;//internal result, stated every while loop
-        boolean nextLayer = true; //is
-        int amountOfRepeat;
-        while (nextLayer) {
-            nextLayer = false;
-            boolean lookup = false;
-            for (int i = 0; i <; i++) {
-                if (!lookup) {
-                    if (s.charAt(i) == '[') {
-                        amountOfRepeat = s.charAt(i - 1);
-                        lookup = true;
-                    }
-                } else
-
-            }
-            for (int i = 0; i < amountOfRepeat; i++) {
-                stringBuilder.append()
-            }
-
-        }
+        return 
     }
 }
-
--- src/test/java/LeetcodeTaskTest.java
+++ src/test/java/LeetcodeTaskTest.java
@@ -3,13 +3,13 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class LeetcodeTaskTest {
     LeetcodeTask leetcodeTask = new LeetcodeTask();
 
     @Test
-    void twoSum() {
-        assertEquals(new int[]{0,1},leetcodeTask.twoSum(new int[]{3,3},6));
-
+    void spiralOrder() {
+        assertEquals(,leetcodeTask.spiralOrder(new int[][]{{11,12,13},{21,22,23},{31,32,33}}));
     }
 }
