7fe295963f79abe5fd86e77f1d2c8f57b6c44227
MRiabov <maksymriabov2004@gmail.com>
2022-07-26 10:50:57 +0100

    Wow, am I fast today! I finished 2 tasks in only 2 hours!...

--- src/main/java/LeetcodeTask.java
+++ src/main/java/LeetcodeTask.java
@@ -3,26 +3,31 @@ import java.util.Collections;
 
 public class LeetcodeTask {
 
-    public String multiply(String num1, String num2) {
-        ArrayList<Integer> digits=new ArrayList<>(num1.length()+num2.length());
-        Collections.fill(digits,0);
-        StringBuilder stringBuilder=new StringBuilder();
-        for (int i=0;i<num1.length();i++)
-            for (int j=0,offset=0;j<num2.length();j++,offset=i+j){
-                digits.set(offset,digits.get(offset)+Character.getNumericValue(num1.charAt(i))*Character.getNumericValue(num1.charAt(j)));
-            }
-        int addToNext=0;
-        int currentPos=0;
-        for (int digit:digits){
-            digits.set(currentPos,(digit+addToNext)%10);
-            stringBuilder.append((digit+addToNext)%10);
-            addToNext=(digit+addToNext)/10;
-            currentPos++;
+    public boolean isPalindrome(ListNode head) {
+        ArrayList<Integer> values = new ArrayList<>();
+        while (head != null) {
+            values.add(head.val);
+            head = head.next;
         }
-
-
-        return stringBuilder.reverse().toString();
+        for (int i = 0; i < values.size() / 2; i++)
+            if (values.get(i) != values.get(values.size() - i - 1)) return false;
+        return true;
     }
 }
 
+class ListNode {
+    int val;
+    ListNode next;
 
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
