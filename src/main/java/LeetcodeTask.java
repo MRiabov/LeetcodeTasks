fa7786381ee7be0e6fa42453cac4e8b8d2b76079
MRiabov <justzed32@gmail.com>
2022-08-09 10:57:40 +0100

    i didn't make this myself, but WOW this is genious.

--- src/main/java/LeetcodeTask.java
+++ src/main/java/LeetcodeTask.java
@@ -1,32 +1,26 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-
 public class LeetcodeTask {
-    int max=0;
-    public int diameterOfBinaryTree(TreeNode root) {
-        dfs(root);
-        return max;
-    }
-    private int dfs(TreeNode currentNode){
-        if (currentNode==null) return 0;
-        int left=dfs(currentNode.left);
-        int right=dfs(currentNode.right);
-        max=Math.max(max,left+right);
-        return Math.max(left,right)+1;
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int start = 0;
+        int end = matrix.length * matrix[0].length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            int row = mid / matrix[0].length;
+            int column = mid % matrix[0].length;
+
+            if (matrix[row][column] == target)
+                return true;
+
+            if (matrix[row][column] < target)
+                start = mid + 1;
+
+            else
+                end = mid - 1;
+        }
+        return false;
+
     }
 }
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() {}
-    TreeNode(int val) { this.val = val; }
-    TreeNode(int val, TreeNode left, TreeNode right) {
-        this.val = val;
-        this.left = left;
-        this.right = right;
-    }
-}
+
--- src/test/java/LeetcodeTaskTest.java
+++ src/test/java/LeetcodeTaskTest.java
@@ -8,11 +8,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class LeetcodeTaskTest {
     LeetcodeTask leetcodeTask = new LeetcodeTask();
 
-
     @Test
-    void longestPalindrome() {
-        assertTrue(leetcodeTask.isBalanced(new TreeNode(3,
-                new TreeNode(9),new TreeNode(20,
-                        new TreeNode(15),new TreeNode(7)))));
+    void searchMatrix() {
+        assertTrue(leetcodeTask.searchMatrix(new int[][]{{1,3}},3));
     }
 }
